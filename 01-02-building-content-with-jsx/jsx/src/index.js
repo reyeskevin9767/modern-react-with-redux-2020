5e689984e32b48feaffd74987005ee59a1c77c32
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-09 18:16:28 -0400

    learning jsx

--- 01-02-building-content-with-jsx/jsx/src/index.js
+++ 01-02-building-content-with-jsx/jsx/src/index.js
@@ -2,9 +2,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//* Reference JS functions in JSX with {}
+// function getButtonText() {
+//   return 'Click on me!';
+// }
+
 //* Create a react component
 const App = () => {
-  return <div>Hi There!</div>;
+  // Reference JS variables in JSX with {}
+  const buttonText = { text: 'click me' };
+  // const style = { backgroundColor: 'blue', color: 'white' }
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      {/* Need to use className for class in JSX */}
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+
+      <input id="name" type="text" />
+
+      {/* Need to use double {} to use styles in JSX */}
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {/* <button style={style}> */}
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 //* Take the react component and show it on the screen
