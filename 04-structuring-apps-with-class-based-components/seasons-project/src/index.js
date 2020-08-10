9edf8ceb664b2930fe3d83a1ac3ba12faf5b4412
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-10 19:35:06 -0400

    refractoring with class component

--- 04-structuring-apps-with-class-based-components/seasons-project/src/index.js
+++ 04-structuring-apps-with-class-based-components/seasons-project/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hit There</div>;
-};
+//* Class Component
+class App extends React.Component {
+  render() {
+    // Get location of user
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
