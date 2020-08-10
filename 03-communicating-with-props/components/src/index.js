124e5c56bbac6322490e8c5040926df008d228e5
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-10 15:59:32 -0400

    create comment detail component

--- 03-communicating-with-props/components/src/index.js
+++ 03-communicating-with-props/components/src/index.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
@@ -21,21 +21,7 @@ const App = () => {
       </div>
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
