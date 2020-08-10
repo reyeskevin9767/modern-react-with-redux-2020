0ebf45feb7d9f598db60be1771e276d939549da9
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-10 16:51:15 -0400

    show component inside another component

--- 03-communicating-with-props/components/src/index.js
+++ 03-communicating-with-props/components/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45PM"
         content="Nice blog post!"
         avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
