e18bab2f5baec588d3c539c896b282637ff953c3
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-10 19:03:04 -0400

    inital project

---
+++ 04-structuring-apps-with-class-based-components/seasons-project/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hit There</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
