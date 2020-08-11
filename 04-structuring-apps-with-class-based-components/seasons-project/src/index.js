072311d281d46f0a08b313b3c81df0058b004fcc
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-11 16:51:06 -0400

    add useState

--- 04-structuring-apps-with-class-based-components/seasons-project/src/index.js
+++ 04-structuring-apps-with-class-based-components/seasons-project/src/index.js
@@ -3,13 +3,40 @@ import ReactDOM from 'react-dom';
 
 //* Class Component
 class App extends React.Component {
-  render() {
+  // Initize state object
+  constructor(props) {
+    // References the parent's constuctor
+    super(props);
+
+    // Onlt time for direct assignment of state
+    this.state = { lat: null, errorMessage: '' };
+
     // Get location of user
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        //Upate state object
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude: </div>;
+  }
+
+  // When setstate is updated, React calls the render method again
+  // Must define render with class component
+  render() {
+    // Show just error message
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    // Show just latitude
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 
