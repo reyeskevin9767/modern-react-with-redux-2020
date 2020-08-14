458fce0cd69889cdc16d1638f2415bfaa630ade7
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-14 12:45:56 -0400

    add async and await

--- 07-handling-user-input/pics/src/components/App.js
+++ 07-handling-user-input/pics/src/components/App.js
@@ -3,8 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ZCs-0-EIyFO_thcTrupXdmrC_4U3CZ79n5FBnlY35-I',
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
