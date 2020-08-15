1abc31b433a78b5dd139e7e007559c4c8fbf7f49
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-15 11:19:25 -0400

    add youtube api and fix axios bug

--- 11-more-react-practice/videos/src/components/App.js
+++ 11-more-react-practice/videos/src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube, {baseParams} from '../apis/youtube';
 
 class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get('/search', {
+      params: {
+        ...baseParams,
+        q: term,
+      },
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
