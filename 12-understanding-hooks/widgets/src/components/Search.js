f38130a1731324cada91c509d6903ddf9d960325
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-17 19:20:39 -0400

    add api

--- 12-understanding-hooks/widgets/src/components/Search.js
+++ 12-understanding-hooks/widgets/src/components/Search.js
@@ -4,10 +4,20 @@ import axios from 'axios';
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  console.log('I run everytime');
-
   useEffect(() => {
-    console.log('I run after term changes');
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
   }, [term]);
 
   return (
