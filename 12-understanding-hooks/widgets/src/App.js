108f527468b0af21f734a5256cb84a68bf17728d
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-22 16:33:31 -0400

    show all components

--- 12-understanding-hooks/widgets/src/App.js
+++ 12-understanding-hooks/widgets/src/App.js
@@ -34,10 +34,35 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showComponent = (route, component) => {
+  return window.location.
+}
+
 const App = () => {
   return (
     <div>
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
