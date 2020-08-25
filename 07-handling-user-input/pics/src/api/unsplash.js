f6c99e54d9399870eb7416a609198b05dd86765b
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 19:44:18 -0400

    move to env file

--- 07-handling-user-input/pics/src/api/unsplash.js
+++ 07-handling-user-input/pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID ZCs-0-EIyFO_thcTrupXdmrC_4U3CZ79n5FBnlY35-I',
+    Authorization: process.env.REACT_APP_CLIENT_ID,
   },
 });
