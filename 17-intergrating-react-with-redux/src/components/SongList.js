ee023711f678406c103e66c9bc30abb4345cf4d4
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-29 22:22:25 -0400

    add style

--- 17-intergrating-react-with-redux/src/components/SongList.js
+++ 17-intergrating-react-with-redux/src/components/SongList.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Redux store to component props
+
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-export default connect()(SongList);
+// Take all data from state and become props
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+// Config connect with mapStateToProps and link to SongList component
+export default connect(mapStateToProps)(SongList);
+
+// this.props === {songs: state.songs}
