3c91d756cd6d7f5da85c4cdc21d6593936c9a7bb
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-13 11:13:51 -0400

    add memoize

--- 18-async-actions-with-redux-thunk/blog/src/actions/index.js
+++ 18-async-actions-with-redux-thunk/blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Cannot use async and await in action creator,
@@ -19,8 +20,11 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// Call once if data is the same
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
