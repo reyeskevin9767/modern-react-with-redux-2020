cd9198c8343e7202b2716300396de4f60fb733c0
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-14 10:49:38 -0400

    add link

--- 20-streams/client/src/components/App.js
+++ 20-streams/client/src/components/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-  return <div>PageOne <Link to="/pagetwo">Naviagate to page two</Link></div>;
-};
-const PageTwo = () => {
-  return <div>Pagetwo <Link to="/">Naviagate to page one</Link></div>;
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
