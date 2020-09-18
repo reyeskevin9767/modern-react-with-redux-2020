9bd98554d3c74716e9c5e7bec1c706153780d24b
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-18 08:48:08 -0400

    link stream delete to button

--- 20-streams/client/src/components/streams/StreamDelete.js
+++ 20-streams/client/src/components/streams/StreamDelete.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
+
 
 const StreamDelete = () => {
   const actions = (
@@ -16,6 +18,7 @@ const StreamDelete = () => {
         title="Delete Stream"
         content="Are you sure you want to delete this stream"
         actions={actions}
+        onDismiss={() => history.push('/')}
       />
     </div>
   );
