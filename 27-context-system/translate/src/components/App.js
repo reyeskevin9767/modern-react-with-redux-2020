fbd04f8ad3b6916f486301e0f195c19b568c7192
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-19 09:25:07 -0400

    create compoenents and context

--- 27-context-system/translate/src/components/App.js
+++ 27-context-system/translate/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguage = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguage('english')}></i>
+          <i className="flag nl" onClick={() => this.onLanguage('dutch')}></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
 
 export default App;
